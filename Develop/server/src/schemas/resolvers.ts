f52c8602f260a1068ef7c5da3934d450a1d4e992
LiebeAnnette <liebeannette@gmail.com>
2025-05-04 08:27:38 -0500

    finished client side instructions and server is running

--- Develop/server/src/schemas/resolvers.ts
+++ Develop/server/src/schemas/resolvers.ts
@@ -1,58 +1,88 @@
-// FROM LESSONS
-// Query: {
-//   me: async (_parent, _args, context) => {
-//     if (!context.req.user) {
-//       throw new AuthenticationError("You must be logged in!");
-//     }
-//     // Fetch and return user info
-//   };
-// }
+import User from "../models/User.js"; // ✅ Correct default import
+import { GraphQLError } from "graphql";
 
-// import { saveBook } from "../controllers/user-controller";
-
-// messing around here while re-watching lessons
-// import { bookSchema } from '../models/book.js';
-import { User } from "../models/index";
-import { AuthenticationError, signToken } from "../services/auth";
-
-interface ILoginUser {
-  email: string;
-  password: string;
-}
-
-interface IAdduserArgs {
-  username: string;
-  email: string;
-  password: string;
-}
-
-interface ISaveBookArgs {
-  authors: string[];
-  description: string;
-  title: string;
-  bookId: string;
-  image: string;
-  link: string;
-}
-
-interface IRemoveBookArgs {
-  bookId: string;
-}
+// import { GraphQLError } from "@apollo/server/errors";
+import { signToken } from "../services/auth.js";
 
 const resolvers = {
   Query: {
     me: async (_parent: any, _args: any, context: any) => {
-      if (!context.req.user) {
-        throw new AuthenticationError("You must be logged in!");
+      if (!context.user) {
+        throw new GraphQLError("You must be logged in!");
       }
-      // Fetch and return user info
+
+      const user = await User.findById(context.user._id).select(
+        "-__v -password"
+      );
+      return user;
     },
   },
+
   Mutation: {
-    login: async (_parent: any, _args: any, context: any) => {},
-    addUser: async (_parent: any, _args: any, context: any) => {},
-    saveBook: async (_parent: any, _args: any, context: any) => {},
-    removeBook: async (_parent: any, _args: any, context: any) => {},
+    login: async (_parent: any, { email, password }: any) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw new GraphQLError("Can't find this user");
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+      if (!correctPw) {
+        throw new GraphQLError("Wrong password!");
+      }
+
+      const token = signToken(user.username, user.email, user._id);
+      return { token, user };
+    },
+
+    addUser: async (_parent: any, { username, email, password }: any) => {
+      const user = await User.create({ username, email, password });
+
+      if (!user) {
+        throw new GraphQLError("Something went wrong!");
+      }
+
+      const token = signToken(user.username, user.email, user._id);
+      return { token, user };
+    },
+
+    saveBook: async (
+      _parent: any,
+      { authors, description, title, bookId, image, link }: any,
+      context: any
+    ) => {
+      if (!context.user) {
+        throw new GraphQLError("You must be logged in!");
+      }
+
+      const updatedUser = await User.findByIdAndUpdate(
+        context.user._id,
+        {
+          $addToSet: {
+            savedBooks: { authors, description, title, bookId, image, link },
+          },
+        },
+        { new: true, runValidators: true }
+      );
+
+      return updatedUser;
+    },
+
+    removeBook: async (_parent: any, { bookId }: any, context: any) => {
+      if (!context.user) {
+        throw new GraphQLError("You must be logged in!");
+      }
+
+      const updatedUser = await User.findByIdAndUpdate(
+        context.user._id,
+        {
+          $pull: { savedBooks: { bookId } },
+        },
+        { new: true }
+      );
+
+      return updatedUser;
+    },
   },
 };
 
